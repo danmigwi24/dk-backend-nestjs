7d87d9a4e92542b3d02ea8cd44d906650a522962
MacBookPro <kimani.daniel@eclectics.io>
2024-01-01 17:36:24 +0300

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,18 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
+
 
 @Controller("api")
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+//
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-
-
+//
   @Post("register")
   async register(
     @Body('name') name:string, 
@@ -20,16 +20,56 @@ export class AppController {
     @Body('email') email:string, 
     @Body('password') password:string, 
     ){
+      const saltOrRounds = 12;
+      const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+      
+      //console.log(`PASSWORD: ${password}`);
+      console.log(`HASHED PASSWORD: ${hashedPassword}`);
 
-      const hashedPassword = await bcrypt.hash(password,12)
-
-      const requestBody = this.appService.register({name,phonenumber,email,hashedPassword})
+      const requestBody = this.appService.register(
+       {
+          name,
+          phonenumber,
+          email,
+          password:hashedPassword
+        })
       return requestBody
   }
+
+  //
+  @Post("login")
+  async login(
+    @Body('phonenumber') phonenumber:string,
+    @Body('password') password:string, 
+  ){
+
+   // const user = await this.appService.login({phonenumber})
+    const user = await this.appService.findOneBy(phonenumber)
+      if(!user){
+        throw new BadRequestException("Invalid credentials")
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      console.log(`USER: ${JSON.stringify(user,null,2)}`);
+      if (isMatch){
+        delete user.password;
+        return user
+      }else{
+        throw new BadRequestException("Invalid credentials") 
+      }
+  }
+//
+  @Post('getallusers')
+  async getAllUsers(){
+    const users = await this.appService.getAllUsers()
+    return users
+  }
+//
+  @Post('findById')
+  async findById(
+    @Body('phonenumber') phonenumber:string,
+  ){
+    const users = await this.appService.findOneBy(phonenumber)
+    return users
+  }
+
 }
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/danmigwi24/dk-backend-nestjs.git
-git push -u origin main
--- src/app.service.ts
+++ src/app.service.ts
@@ -6,15 +6,28 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class AppService {
   constructor(
-    @InjectRepository(User) private readonly userRepository:Repository<User>
-  ){}
-  
+    @InjectRepository(User) private readonly userRepository: Repository<User>
+  ) { }
+
   getHello(): string {
     return 'Hello World!';
   }
 
-  async register(data:any):Promise<User>{
-return this.userRepository.save(data)
+  async register(data: any): Promise<User> {
+    console.log(`DATA \n ${JSON.stringify(data,null,2)}`)
+    return this.userRepository.save(data);
   }
-  
+
+  async login(data: any): Promise<User | null> {
+    return this.userRepository.findOne(data);
+  }
+
+  async findOneBy(phonenumber: string): Promise<User | null> {
+    return this.userRepository.findOneBy({ phonenumber });
+  }
+
+
+  async getAllUsers(): Promise<User[]> {
+    return this.userRepository.find()
+  }
 }
